7c8c3b6820a64dcbaa32fd1b37129785c0b63574
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-02-02 17:44:02 +0900

    編集/削除機能追加

--- app/controllers/twts_controller.rb
+++ app/controllers/twts_controller.rb
@@ -1,20 +1,24 @@
 class TwtsController < ApplicationController
-  def index
-    @twts = Twt.all
+
+  before_action :_set_twt_with_id, only: [:edit, :update, :destroy]  
+  def destroy
+    @twt.destroy
+    redirect_to twts_path, notice:"ブログを削除しました！"
+  end
+  
+  def edit
   end
 
-  def create
-    @twt = Twt.new(_twt_params)
-    if params[:back]
-      render :new
+  def update
+    if @twt.update(_twt_params)
+      redirect_to twts_path, notice: "ツイートを編集しました!"
     else
-      if @twt.save
-        redirect_to new_twt_path, notice: '正常に保存しました'
-      else
-        notice='保存に失敗しました'
-        render :new
-      end
+      render :edit
     end
+  end
+
+  def index
+    @twts = Twt.all.order(:id)
   end
 
   def new
@@ -26,8 +30,27 @@ class TwtsController < ApplicationController
     render :new if @twt.invalid?
   end
 
+
+  def create
+    @twt = Twt.new(_twt_params)
+    if params[:back]
+      render :new
+    else
+      if @twt.save
+        redirect_to new_twt_path, notice: 'ツイートを保存しました!'
+      else
+        notice='保存に失敗しました'
+        render :new
+      end
+    end
+  end
+
   private
   def _twt_params
     params.require(:twt).permit(:content)
+  end
+
+  def _set_twt_with_id
+    @twt=Twt.find(params[:id])
   end
 end
--- app/helpers/twts_helper.rb
+++ app/helpers/twts_helper.rb
@@ -1,2 +1,9 @@
 module TwtsHelper
+  def choose_new_or_edit
+    if action_name=='new' || action_name=='create'
+      { uri: confirm_twts_path, header: '新規作成', label: 'ツイートする' }
+    elsif action_name=='edit'
+      { uri: twt_path, header: '編集', label: '編集結果を保存する' }
+    end
+  end
 end
