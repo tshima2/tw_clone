8499f4d44fde5e4ea51da8b624276fddc4f99007
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-02-02 20:22:43 +0900

    アクション名の記載漏れを追加

--- app/helpers/twts_helper.rb
+++ app/helpers/twts_helper.rb
@@ -1,8 +1,8 @@
 module TwtsHelper
   def choose_new_or_edit
-    if action_name=='new' || action_name=='create'
+    if action_name=='new' || action_name=='create' || action_name=='confirm'
       { uri: confirm_twts_path, header: '新規作成', label: 'ツイートする' }
-    elsif action_name=='edit'
+    elsif action_name=='edit' || action_name=='update'
       { uri: twt_path, header: '編集', label: '編集結果を保存する' }
     end
   end
